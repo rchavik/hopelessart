2f57eba415daa7fe0a7cf034d55a1e40212203f3
Pascal Precht <pascal.precht@web.de>
2011-01-11 15:40:00 +0100

    Added some comments

--- hopelessart/helpers/custom.php
+++ hopelessart/helpers/custom.php
@@ -1,9 +1,25 @@
 <?php
-
+/**
+ * CustomHelper class for HopelessArt- theme
+ * 
+ * @package helpers
+ */
 class CustomHelper extends AppHelper {
 
+/**
+ * Instance of View
+ * 
+ * @var View
+ * @access public
+ */
 	var $View = null;
 
+/**
+ * Array of available social networks
+ * 
+ * @var array
+ * @access public
+ */
 	var $_social_icons = array(
 		'twitter',
 		'rss',
@@ -17,10 +33,25 @@ class CustomHelper extends AppHelper {
 		'technorati'
 	);
 
+/**
+ * Constructor
+ * 
+ * @access public
+ * @return void
+ */
 	function CustomHelper() {
 		$this->View =& ClassRegistry::getObject('view');
 	}
 
+/**
+ * Method to display social-bookmarks
+ * 
+ * @param string $type - Type of social-bookmark
+ * @param string $link - Link to you social bookmark site
+ * 
+ * @access public
+ * @return string social-bookmark HTML element
+ */
 	function socialBookmark($type, $link) {
 		$output = '';
 		if (in_array($type, $this->_social_icons)) {
